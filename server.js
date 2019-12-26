b6f8df3f12257fba25a1bc9612aef884a8f13fa5
Keegan Nair <keegan.nair@hotmail.com>
2019-12-26 14:27:44 +1100

    adding server port but not connecting cuz of error

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
