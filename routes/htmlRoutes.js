8216e08527e730867106b5f7ee9cad21b005fedf
Keegan Nair <keegan.nair@hotmail.com>
2019-12-26 14:30:51 +1100

    server port working now but CSS not applying

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,19 @@
 var path = require("path");
-var router = require("express").Router();
+// var router = require("express").Router();
 
 // "/notes" responds with the notes.html file
-router.get("/notes", function (req, res) {
+
+module.exports = function(app) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 // All other routes respond with the index.html file
-router.get("*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+}
 
-module.exports = router;
+// module.exports = router;
 
 //
