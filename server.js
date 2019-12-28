71a944ca04fbec9238a76b13628f517e9afbd6f6
Keegan Nair <keegan.nair@hotmail.com>
2019-12-28 15:25:00 +1100

    script not connecting properly

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
